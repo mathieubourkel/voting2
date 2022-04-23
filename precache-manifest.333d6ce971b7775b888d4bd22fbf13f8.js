0572c94b97f329806e27d0e5c62fd8e342f1a9ab
Mathieu Bourkel <mathieu.bourkel@gmail.com>
2022-04-23 20:52:26 +0200

    Updates

---
+++ precache-manifest.333d6ce971b7775b888d4bd22fbf13f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255eff90209c7f218d78fe8d60966845",
+    "url": "/voting2/index.html"
+  },
+  {
+    "revision": "af8bb60d556f73502607",
+    "url": "/voting2/static/css/2.b6834657.chunk.css"
+  },
+  {
+    "revision": "ec55939a3a6ff3dce96b",
+    "url": "/voting2/static/css/main.c8268d45.chunk.css"
+  },
+  {
+    "revision": "af8bb60d556f73502607",
+    "url": "/voting2/static/js/2.2968b533.chunk.js"
+  },
+  {
+    "revision": "ec55939a3a6ff3dce96b",
+    "url": "/voting2/static/js/main.662553d0.chunk.js"
+  },
+  {
+    "revision": "4b8980531bb35b526abc",
+    "url": "/voting2/static/js/runtime-main.74737a20.js"
+  }
+]);
